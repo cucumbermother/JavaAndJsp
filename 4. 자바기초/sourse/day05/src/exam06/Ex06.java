994ccf82fc77b0d0647dd0ab218abdc1679b65f8
cucumbermother <duwkrladbsk2@naver.com>
2024-10-14 21:30:53 +0900

    day05

---
+++ 4. 자바기초/sourse/day05/src/exam06/Ex06.java
@@ -0,0 +1,15 @@
+package exam06;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        add(10,20);
+        System.out.println(add(10,20));
+    }
+    static int add(int num1, int num2){
+        try {
+            return num1 + num2; // 함수 종료
+        } finally {
+            System.out.println("정상 무조건 실행!");
+        }
+    }
+}
